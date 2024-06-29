2ccc909f599cd9b55089f5afb7f8414a841109d6
arafatGithubCode <md489288@gmail.com>
2024-06-29 16:41:40 +0600

    add mongodb

---
+++ models/User.ts
@@ -0,0 +1,16 @@
+import mongoose, { Document, Model, Schema } from "mongoose";
+
+interface IUser extends Document {
+  name: string;
+  email: string;
+}
+
+const userSchema: Schema<IUser> = new Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+});
+
+const User: Model<IUser> =
+  mongoose.models.User || mongoose.model<IUser>("User", userSchema);
+
+export default User;
